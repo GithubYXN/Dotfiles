b68437f317151892b60d153923c82f4ff5a76784
GithubYXN <2502869657@qq.com>
2025-05-09 10:54:16 +0800

    update neovim config

---
+++ nvim/lua/core/basic.lua
@@ -0,0 +1,36 @@
+-- set line number
+vim.opt.number = true
+
+-- set relative number
+vim.opt.relativenumber = true
+
+-- set current line highlight
+vim.opt.cursorline = true
+
+-- set column width
+vim.opt.colorcolumn = "80"
+
+-- set tab to 4 spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+
+-- auto reload while changed outside
+vim.opt.autoread = true
+
+-- set leader
+vim.g.mapleader = " "
+
+-- disable mode display
+vim.opt.showmode = false
+
+-- enable clipboard share
+vim.opt.clipboard = "unnamedplus"
+
+-- case insensitive if there is no uppercase in content
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- cancle matched result highlight after search
+vim.opt.hlsearch = false
+
